8be26e98da1fc7f4c9c8e0e492383109cad7e773
bardiya2254kariminia <135696596+bardiya2254kariminia@users.noreply.github.com>
2023-07-01 08:35:31 +0330

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,24 @@
+import network.Server;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Formatter;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+//        new Server().start();
+        Socket socket=new Socket("127.0.0.1",8000);
+        DataOutputStream dos=new DataOutputStream(socket.getOutputStream());
+        DataInputStream dis=new DataInputStream(socket.getInputStream());
+//        dos.writeUTF("");
+//        dos.flush();
+        dos.writeUTF("getTop5\nusername:salmanian,email:@gmail.com,password:aA111111,genre:Science Fiction,type:a");
+        dos.flush();
+        System.out.println(dis.readUTF());
+//        System.out.println(dis.readUTF());
+        socket.close();
+        dos.close();
+        dos.close();
+    }
+}
---
+++ test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        String str="salam";
+        String s=str.split(",")[0];
+        System.out.println(s);
+
+    }
+}
